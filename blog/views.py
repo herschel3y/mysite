d9b2c8f24e60058581012095cba4f6631849b539
herschel3y <xiaobleach@gmail.com>
2023-03-26 15:04:26 +0800

    ok

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
+
 
 def post_list(request):
     posts = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+        
     return render(request,
                   'blog/post/list.html',
                   {'posts':posts})
@@ -19,3 +29,19 @@ def post_detail(request,year,month,day,post):
     return render(request,
                   'blog/post/detail.html',
                   {'post':post})
+
+def post_share(request,post_id):
+    #Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        #Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #Form fields passed validation
+            cd = form.cleaned_data
+            #....send email
+    
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html',{'post':post,
+                                                   'form':form})        
