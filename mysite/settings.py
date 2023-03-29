2cc68876881f639f65f266d16dc3e80bb3c27caa
herschel3y <xiaobleach@gmail.com>
2023-03-29 23:54:56 +0800

    okkk
    ok

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/css/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'xiaobleach@gmail.com'
+EMAIL_HOST_PASSWORD = 'altgvvtwqeswyxko'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
